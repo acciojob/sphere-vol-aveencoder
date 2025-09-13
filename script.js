0729859574482a0322e07657525fdccfe9d6fbae
aveencoder <aveen598@gmail.com>
2025-09-13 20:37:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,4 +12,5 @@ const volume = document.getElementById('volume');
   
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload ()=>{ document.getElementById('MyForm').onsubmit = volume_sphere;
+				  }
