e9cf91107f94095becae6d83676a9e3b8df93c79
aveencoder <aveen598@gmail.com>
2025-09-13 18:15:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function volume_sphere() {
-    //Write your code here
-	agjodjoiqefoqoifjo
+	event.preventDefault();
+	
+const radius = document.getElementById('radius');
+const volume = document.getElementById('volume');
+	 const r = Number(radius.value);
+    if(r<=0 || isNaN(r)) {
+       volume.value ="please enter the valid value";
+       return;
+    }
+   volume.value =  (4/3)*Math.PI*Math.pow(r,3);
   
 } 
 
